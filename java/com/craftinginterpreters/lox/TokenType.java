1495b7c5f10081d8499dadc378ba4e84c2a7a759
shuntaka9576 <shuntaka9576@gmail.com>
2023-10-24 05:11:19 +0900

    20231024

---
+++ java/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,19 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+  // 記号1個のトークン
+  LFET_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // 記号1個または2個によるトークン
+  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+  // リテラル
+  IDENTIFIER, STRING, NUBMBER,
+
+  // キーワード
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  EOF
+}
