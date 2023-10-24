b8b8227e7c5fbf1f0ac12c3c7a7fd138accc82ad
shuntaka9576 <shuntaka9576@gmail.com>
2023-10-25 05:12:27 +0900

    20231025

--- java/com/craftinginterpreters/lox/Scanner.java
+++ java/com/craftinginterpreters/lox/Scanner.java
@@ -74,10 +74,68 @@ class Scanner {
       case '>':
         addToken(match('=') ? GREATER_EQUAL : GREATER);
         break;
+      case '/':
+        if (match('/')) {
+          // コメントは行末まで続く
+          while (peek() != '\n' && !isAtEnd())
+            advance();
+        } else {
+          addToken(SLASH);
+        }
+      case ' ':
+      case '\r':
+      case '\t':
+        break;
+      case '\n':
+        line++;
+        break;
+      case '"':
+        string();
+        break;
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
+
         break;
     }
+  }
+
+  private void number() {
+    while (isDigit(peek()))
+      advance();
+
+    // 小数点を消費するのは、小数点記号の後に数値があることを確認したあとだけ
+    if (peek() == '.' && isDigit(peekNext())) {
+      advance(); // 小数点を消費
+
+      while (isDigit(peek()))
+        advance();
+    }
+
+    // 字句を数値に変換
+    addToken(NUBMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n')
+        line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    advance(); // 右側の引用符を消費
+
+    // 左右の"を切り落とす
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
   }
 
   private boolean match(char expected) {
@@ -88,6 +146,22 @@ class Scanner {
 
     current++;
     return true;
+  }
+
+  private char peek() {
+    if (isAtEnd())
+      return '\0';
+    return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length())
+      return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
   }
 
   private boolean isAtEnd() {
