245c26ce4f87d077f9998439c75bed552ab40c61
shuntaka9576 <shuntaka9576@gmail.com>
2024-12-26 17:39:49 +0900

    20241225

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -21,6 +21,14 @@ class Interpreter implements Expr.Visitor<Object> {
     Object right = evaluate(expr.right);
 
     switch (expr.operator.type) {
+      case GREATER:
+        return (double) left > (double) right;
+      case GREATER_EQUAL:
+        return (double) left >= (double) right;
+      case LESS:
+        return (double) left < (double) right;
+      case LESS_EQUAL:
+        return (double) left <= (double) right;
       case MINUS:
         return (double) left - (double) right;
       case PLUS:
