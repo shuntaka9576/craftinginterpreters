bb0648a9c69e35004fb0f92361788e1bbb08bf46
shuntaka9576 <shuntaka9576@gmail.com>
2025-01-01 06:54:58 +0900

    20250101

--- clox/chunk.c
+++ clox/chunk.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "chunk.h"
+#include "memory.h"
 
 void initChunk(Chunk *chunk) {
   chunk->count = 0;
@@ -8,4 +9,13 @@ void initChunk(Chunk *chunk) {
   chunk->code = NULL;
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte) {}
+void writeChunk(Chunk *chunk, uint8_t byte) {
+  if (chunk->capacity < chunk->count + 1) {
+    int oldCpacity = chunk->capacity;
+    chunk->capacity = GROW_CAPACITY(oldCpacity);
+    chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCpacity, chunk->capacity);
+  }
+
+  chunk->code[chunk->count] = byte;
+  chunk->count++;
+}
